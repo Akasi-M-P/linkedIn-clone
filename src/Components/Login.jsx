7cf10481ded59502696cc445a921369304c284d5
Mba96 <mbajameel90@gmail.com>
2023-04-13 22:53:47 +0000

    started working on the Hero section

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -13,48 +13,52 @@ const Login = (props) => {
           <SignIn>Sign In</SignIn>
         </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+          <img src="/images/hero.svg" alt="" />
+        </Hero>
+      </Section>
     </Container>
   );
-}
-
+};
 
 const Container = styled.div`
-    padding: 0px;
+  padding: 0px;
 `;
 
 const Nav = styled.nav`
-    max-width: 1128;
-    margin: auto;
-    padding: 12px 0 16px;
-    display: flex;
-    align-items: center;
-    position: relative;
-    justify-content: space-between;
-    flex-wrap: nowrap;
+  max-width: 1128;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
 
-    & > a {
-        width: 135px;
-        height: 34px;
-        @media (max-width:768px) {
-            padding: 0 5px;
-          
-        }
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
     }
+  }
 `;
 
 const Join = styled.a`
-    font-size: 16px;
-    padding: 10px 12px;
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  border-radius: 1rem;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
     text-decoration: none;
-    border-radius: 1rem;
-    color: rgba(0, 0, 0, 0.6);
-    margin-right: 12px;
-    cursor: pointer;
-    &:hover {
-        background-color: rgba(0, 0, 0, 0.08);
-        color: rgba(0, 0, 0, 0.9);
-        text-decoration: none;
-    }
+  }
 `;
 
 const SignIn = styled.a`
@@ -68,12 +72,78 @@ const SignIn = styled.a`
   padding: 10px 24px;
   text-align: center;
   background-color: rgba(0, 0, 0, 0);
-  cursor:pointer;
+  cursor: pointer;
 
   &:hover {
     background-color: rgba(112, 181, 249, 0.15);
     font-color: #0a66c2;
     text-decoration: none;
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-items: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (min-width: 768px) {
+    margin: 0;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    color: #8f5849;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
+      Roboto, "Helvetica Neue", "Fira Sans", Ubuntu, Oxygen, "Oxygen Sans",
+      Cantarell, "Droid Sans", "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Emoji", "Segoe UI Symbol", "Lucida Grande", Helvetica, Arial,
+      sans-serif;
+    font-size: 56px;
+    font-weight: 200;
+
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+     top: 130px;
+    @media (max-width: 768px) {
+        top: 230px;
+        width: 328px;
+        position: initial;
+        height: initial;
+        padding-left: 112px;
+        padding-right: 112px;
+         padding-top: 24px;
+}
+    }
+
+  
+    }
   }
 `;
 
